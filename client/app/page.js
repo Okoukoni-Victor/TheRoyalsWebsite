acce33e25fc928578f3996fb16d34e60027839ef
Bukola Laniyan <105382504+khenzee@users.noreply.github.com>
2026-04-20 15:26:48 +0100

    landing page

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Hero from "../components/landingPage/hero/Hero";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="font-helixa ">Home is here to stay</h1>
+      <Hero />
     </main>
   );
 }
